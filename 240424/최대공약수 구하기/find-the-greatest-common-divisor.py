3da6e6c3fea332b0e61d201371575e2c0b6a1adf
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-04-24 22:36:28 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240424/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240424/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,12 +1,11 @@
 n,m=map(int,input().split())
 def func(n,m):
-    for num in range(max(n,m),1,-1):
-        if m==1 or n==1:
-            print(1)
-            return
+    for num in range(max(n,m),0,-1):
 
         if m%num==0 and n%num==0:
             print(num)
             return
+        elif 
+
 
 func(n,m)
