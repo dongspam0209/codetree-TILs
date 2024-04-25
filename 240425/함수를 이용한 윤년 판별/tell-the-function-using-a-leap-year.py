71b2b50801933c98e00eb951978e8e823d894bb1
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-04-25 16:28:37 +0900

    Title: 함수를 이용한 윤년 판별, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240425/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240425/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -3,8 +3,9 @@ def func(y):
     check=True
     if y%4==0:
         return check
-    if y%100==0 and y%400!=0:
-        return check
+        if y%100==0 and y%400!=0:
+            check=False
+            return check
     else:
         check=False
         return check
