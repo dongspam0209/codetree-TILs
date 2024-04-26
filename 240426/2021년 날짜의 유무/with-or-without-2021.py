01770588336804b3e72643df83b9e220909a559c
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-04-26 15:40:03 +0900

    Title: 2021년 날짜의 유무, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240426/2021년 날짜의 유무/with-or-without-2021.py
+++ 240426/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,9 +1,13 @@
 M,D=map(int,input().split())
 def func(M,D):
-    day_arr=[0,31,28,31,30,31,30,31,31,30,31,30,31]
-    if day_arr[M]<D:
+    if M>12:
         print("No")
+        return
     else:
-        print("Yes")
+        day_arr=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+        if day_arr[M]<D:
+            print("No")
+        else:
+            print("Yes")
 
 func(M,D)
