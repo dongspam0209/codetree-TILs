09668993cb2ff40a5de7593f2aa2c2de576ad28d
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-04-29 16:01:41 +0900

    Title: 사는 지역, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240429/사는 지역/where-live.py
+++ 240429/사는 지역/where-live.py
@@ -9,6 +9,13 @@ for _ in range(n):
     name,street,region=input().split()
     people_list.append(People(name,street,region))
 
-print('name',people_list[-1].name)
-print('addr',people_list[-1].street)
-print('city',people_list[-1].region)
+name_list=[]
+for person in people_list:
+    name_list.append(person.name)
+    name_list.sort(reverse=True)
+
+for person in people_list:
+    if person.name==name_list[0]:
+        print('name',person.name)
+        print('addr',person.street)
+        print('city',person.region)
