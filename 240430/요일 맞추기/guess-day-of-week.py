7a796d71af399c6ade0659ca7787e0d1e609f9eb
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-04-30 16:01:15 +0900

    Title: 요일 맞추기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240430/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,13 @@
+m_1,d_1,m_2,d_2=map(int,input().split())
+arr=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+A=sum(arr[:m_1])+d_1
+B=sum(arr[:m_2])+d_2
+difference=B-A
+if difference<0:
+    count=abs(difference)%7
+    weekday_list=['Mon','Sun','Sat','Fri','Thu','Wed','Fri']
+    print(weekday_list[count])
+else:
+    count=abs(difference)%7
+    weekday_list=['Mon','Fri','Wed','Thu','Fri','Sat','Sun']
+    print(weekday_list[count])
