a5ebb55bf28f4306d7bad42bc611d79acf7a4398
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-01 20:32:56 +0900

    Title: 체크판위에서 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/체크판위에서 2/on-the-checkboard-2.py
+++ 240501/체크판위에서 2/on-the-checkboard-2.py
@@ -1,24 +1,19 @@
 R,C=map(int,input().split())
 arr=[]
-cnt=0
 for _ in range(R):
     arr.append(list(input().split()))
-path=[[]]
-cur_node=arr[0][0]
-for i in range(R):
-    for j in range(C):
-        temp_path=[(0,0)]
-        for k in range(i+1,R):
-            for l in range(j+1,C):
-                if arr[k][l]!=cur_node:
-                    temp_path.append((k,l))
-                    j=l
-                    cur_node=arr[k][l]
-                    break
-        if temp_path[-1]==(R-1,C-1):
-            path.append(temp_path)
 
-for idx in range(len(path)):
-    if len(path[idx])==4:
-        cnt+=1
-print(cnt)
+next_node=[]
+next_next_node=[]
+for i in range(1,R):
+    for j in range(1,C):
+        if arr[i][j]!=arr[0][0] and not(i==R-1 and j==C-1):
+            next_node.append((i,j))
+
+for i,j in next_node:
+    for k in range(i+1,R-1):
+        for l in range(j+1,C-1):
+            if arr[i][j]!=arr[k][l] and not(i==R-1 and j==C-1):
+                next_next_node.append((k,l))
+
+print(len(next_next_node))
