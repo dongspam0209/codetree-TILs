d694890f652beecf8e146f941dc9ba22d7a661db
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-01 22:27:27 +0900

    Title: 원 모양으로 되어있는 방, Time: 137ms, Memory: 34MB, Status: Passed - CodeTree

--- 240501/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240501/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -2,15 +2,17 @@ N=int(input())
 space=[]
 for _ in range(N):
     space.append(int(input()))
+
 distance_list=[]
 for room in range(N):
     distance=0
     for idx in range(N):
         distance_factor=0
         if idx<room:
-            distance_factor=(N-1)-room+abs(room-idx)
+            distance_factor=N-room+idx
         else:
             distance_factor=idx-room
+        # print(distance_factor,end=" ")
         distance+=distance_factor*space[idx]
     distance_list.append(distance)
 
