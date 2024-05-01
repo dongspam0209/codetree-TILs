4b20c3502b90b054516f44d3d5cde6f03018dbd0
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-01 20:36:35 +0900

    Title: 체크판위에서 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/체크판위에서 2/on-the-checkboard-2.py
+++ 240501/체크판위에서 2/on-the-checkboard-2.py
@@ -2,18 +2,20 @@ R,C=map(int,input().split())
 arr=[]
 for _ in range(R):
     arr.append(list(input().split()))
-
+if arr[0][0]==arr[-1][-1]:
+    print(0)
+    exit()
+    
 next_node=[]
 next_next_node=[]
 for i in range(1,R):
     for j in range(1,C):
         if arr[i][j]!=arr[0][0] and not(i==R-1 and j==C-1):
             next_node.append((i,j))
-
+print(next_node)
 for i,j in next_node:
     for k in range(i+1,R-1):
         for l in range(j+1,C-1):
             if arr[i][j]!=arr[k][l] and not(i==R-1 and j==C-1):
                 next_next_node.append((k,l))
-
-print(len(next_next_node))
+print(next_next_node)
