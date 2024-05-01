bac488b70bba038fd8087979d17a3886a0041759
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-01 20:37:04 +0900

    Title: 체크판위에서 2, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240501/체크판위에서 2/on-the-checkboard-2.py
+++ 240501/체크판위에서 2/on-the-checkboard-2.py
@@ -5,17 +5,17 @@ for _ in range(R):
 if arr[0][0]==arr[-1][-1]:
     print(0)
     exit()
-    
+
 next_node=[]
 next_next_node=[]
 for i in range(1,R):
     for j in range(1,C):
         if arr[i][j]!=arr[0][0] and not(i==R-1 and j==C-1):
             next_node.append((i,j))
-print(next_node)
+# print(next_node)
 for i,j in next_node:
     for k in range(i+1,R-1):
         for l in range(j+1,C-1):
             if arr[i][j]!=arr[k][l] and not(i==R-1 and j==C-1):
                 next_next_node.append((k,l))
-print(next_next_node)
+print(len(next_next_node))
