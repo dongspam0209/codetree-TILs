2219fd13838aa1254aa00f6a270033923f85691c
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-02 12:00:24 +0900

    Title: Carry 피하기 2, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,38 @@
+import sys
+MAX_INT=-sys.maxsize
+
+n=int(input())
+arr=[]
+for _ in range(n):
+    arr.append(int(input()))
+
+def calculate_carry(a,b,c):
+    calculate_result=[]
+    while a>0 or b>0 or c>0:
+        position_sum=0
+        position_sum=a%10+b%10+c%10
+        if position_sum>=10:
+            calculate_result.append(-1)
+            break
+        calculate_result.append(position_sum)
+        a//=10
+        b//=10
+        c//=10
+    return calculate_result
+
+sum_list=[]
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            a,b,c=arr[i],arr[j],arr[k]
+            sum_list.append(calculate_carry(a,b,c))
+
+for ans in sum_list:
+    if ans[-1]==-1:
+        continue
+    max_num=0
+    for idx in range(len(ans)):
+        max_num+=ans[idx]*10**idx
+    MAX_INT=max(max_num,MAX_INT)
+
+print(MAX_INT)
