388b23c98b232b8f18cf45b699d1538eda10fde4
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-02 19:05:27 +0900

    Title: 최고의 13위치 2, Time: 182ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/최고의 13위치 2/best-place-of-13-2.py
+++ 240502/최고의 13위치 2/best-place-of-13-2.py
@@ -8,9 +8,8 @@ for _  in range(N):
 for i in range(N):
     for j in range(N-2):
         for k in range(N):
-            sum=0
             for l in range(N-2):
-                if (k,l)==(i,j) or (k,l)==(i,j+1) or (k,l)==(i,j+2):
+                if (k,l)==(i,j) or (k,l)==(i,j+1) or (k,l)==(i,j+2) or (k,l+2)==(i,j) or (k,l+2)==(i,j+1):
                     continue
                 sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[k][l]+arr[k][l+1]+arr[k][l+2]
                 MAX_INT=max(MAX_INT,sum)
