21348f1f0ba7d453cd586f3cc1d7352057f1ee01
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-02 11:12:00 +0900

    Title: 씨 오 더블유 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/씨 오 더블유 2/c-o-w-2.py
+++ 240502/씨 오 더블유 2/c-o-w-2.py
@@ -2,8 +2,8 @@ N=int(input())
 arr=input()
 cnt=0
 for i in range(N):
-    for j in range(N):
-        for k in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
             if arr[i]=='C' and arr[j]=='O' and arr[k]=='W':
                 cnt+=1
 
