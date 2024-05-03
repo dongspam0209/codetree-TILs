023ac3d21045f10b2068a058b4c7a199747b5258
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-03 22:56:31 +0900

    Title: 바구니 안의 사탕 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240503/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -9,7 +9,7 @@ for i in range(100-2*K+1):
     temp_sum=0
     for j in range(i,i+2*K+1):
         temp_sum+=arr[j]
-        MAX_INT=max(temp_sum,MAX_INT)
+    MAX_INT=max(temp_sum,MAX_INT)
 
 
 print(MAX_INT)
