e674d37594279ca30c7ce6dde70739642a786a74
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-03 22:55:53 +0900

    Title: 바구니 안의 사탕 2, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240503/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,7 +1,7 @@
 import sys
 MAX_INT=-sys.maxsize
 N,K=map(int,input().split())
-arr=[0]*100 # basket position
+arr=[0]*101 # basket position
 for _ in range(N):
     amount,idx=map(int,input().split())
     arr[idx]+=amount
@@ -10,6 +10,6 @@ for i in range(100-2*K+1):
     for j in range(i,i+2*K+1):
         temp_sum+=arr[j]
         MAX_INT=max(temp_sum,MAX_INT)
-        
+
 
 print(MAX_INT)
