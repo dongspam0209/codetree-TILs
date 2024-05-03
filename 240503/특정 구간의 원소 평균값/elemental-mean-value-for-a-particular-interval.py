0024cf7e89bfe02af2cd722a04a0924708d7e4b5
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-03 21:00:51 +0900

    Title: 특정 구간의 원소 평균값, Time: 125ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240503/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240503/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -4,9 +4,10 @@ cnt=0
 for i in range(N):
     for j in range(i+1,N):
         partial_sum=0
-        for k in range(i,j+1):
-            partial_sum+=arr[k]
-        mean=partial_sum//(j+1-i)
-        if mean in arr[i:j+1]:
-            cnt+=1
+        if i!=j:
+            for k in range(i,j+1):
+                partial_sum+=arr[k]
+            mean=partial_sum//(j+1-i)
+            if mean in arr[i:j+1]:
+                cnt+=1
 print(cnt)
