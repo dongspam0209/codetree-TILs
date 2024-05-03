8854b1956206530862e67a8baa54f1370aef1d2f
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-03 22:51:46 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240503/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,15 @@
+import sys
+MAX_INT=-sys.maxsize
+N,K=map(int,input().split())
+arr=[0]*100 # basket position
+for _ in range(N):
+    amount,idx=map(int,input().split())
+    arr[idx]+=amount
+for i in range(100-2*K):
+    temp_sum=0
+    for j in range(i,i+2*K+1):
+        temp_sum+=arr[j]
+        MAX_INT=max(temp_sum,MAX_INT)
+        
+
+print(MAX_INT)
