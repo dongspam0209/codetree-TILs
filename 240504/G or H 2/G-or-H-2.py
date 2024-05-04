68be34d95400ae9294bb723dec96737c7a87fb27
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-04 16:21:27 +0900

    Title: G or H 2, Time: 270ms, Memory: 26MB, Status: Passed - CodeTree

--- 240504/G or H 2/G-or-H-2.py
+++ 240504/G or H 2/G-or-H-2.py
@@ -7,31 +7,28 @@ for _ in range(N):
     idx=int(idx)
     arr[idx]=word
 
-for k in range(100):
+for k in range(101):
     for i in range(0,101-k):
         G_cnt=0
         H_cnt=0
         ans=0
-        for j in range(i,i+k):
+        for j in range(i,i+k+1):
             if arr[j]=='G':
                 G_cnt+=1
             elif arr[j]=='H':
                 H_cnt+=1
             
-        if G_cnt==H_cnt and arr[i]!='' and arr[i+k-1]!='':
-
+        if G_cnt==H_cnt and arr[i]!='' and arr[i+k]!='':
             # print('k',k,'시작',i,'끝',i+k-1)
-            ans=k-1
+            ans=k
             MAX_INT=max(ans,MAX_INT)
-        elif H_cnt==0 and arr[i]!='' and arr[i+k-1]!='':
-
+        elif H_cnt==0 and arr[i]=='G' and arr[i+k]=='G':
             # print('k',k,'시작',i,'끝',i+k-1)
-            ans=k-1
+            ans=k
             MAX_INT=max(ans,MAX_INT)
-        elif G_cnt==0 and arr[i]!='' and arr[i+k-1]!='':
-
+        elif G_cnt==0 and arr[i]=='H' and arr[i+k]=='H':
             # print('k',k,'시작',i,'끝',i+k-1)
-            ans=k-1
+            ans=k
             MAX_INT=max(ans,MAX_INT)
 
 print(MAX_INT)
