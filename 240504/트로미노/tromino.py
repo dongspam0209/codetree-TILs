d274a3e2dadda359d5e7655b0092986f68eb81ec
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-04 21:43:38 +0900

    Title: 트로미노, Time: 165ms, Memory: 26MB, Status: Passed - CodeTree

--- 240504/트로미노/tromino.py
+++ 240504/트로미노/tromino.py
@@ -24,13 +24,18 @@ for i in range(n):
         MAX_INT=max(case_1,case_2,case_3,case_4,MAX_INT)
         
 for i in range(n):
-    case_1=0
-    case_2=0
-    for j in range(n):
-        if i+3>n or j+3>n:
-            continue
-        case_1=arr[i][j]+arr[i+1][j]+arr[i+2][j]
-        case_2=arr[i][j]+arr[i][j+1]+arr[i][j+1]
+    for j in range(m):
+        case_1=0
+        case_2=0
+        for k in range(i,i+3):
+            if i+3>n:
+                continue
+            case_1+=arr[k][j]
+        MAX_INT=max(case_1,MAX_INT)
+        for l in range(j,j+3):
+            if j+3>m:
+                continue
+            case_2+=arr[i][l]
         MAX_INT=max(case_1,case_2,MAX_INT)
 
 print(MAX_INT)
