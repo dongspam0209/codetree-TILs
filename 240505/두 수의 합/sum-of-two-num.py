2dec3c667cdc5060236b25943b068f0a5896a3d8
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-05-05 19:26:45 +0900

    Title: 두 수의 합, Time: 141ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240505/두 수의 합/sum-of-two-num.py
+++ 240505/두 수의 합/sum-of-two-num.py
@@ -2,11 +2,16 @@ from collections import defaultdict
 n,k=map(int,input().split())
 arr=list(map(int,input().split()))
 num_dict=defaultdict(list)
+
 for idx,elem in enumerate(arr):
     num_dict[elem].append(idx+1)
+
+
 cnt=0
-for i in num_dict:
-    temp_score=k-i
-    if temp_score in num_dict:
-        cnt+=len(num_dict[temp_score])
+for i in range(n):
+    tmp=arr[i]
+    check_score=k-tmp
+    if check_score in num_dict:
+        cnt+=len(num_dict[check_score])-1
+
 print(cnt//2)
